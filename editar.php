af41b50d31651ccbddf3745121b0eab25f2adf58
LuisFOsG <luisfernandogarciaosorio@gmail.com>
2020-11-03 18:08:39 -0500

    Mensaje Validacion Cancelar
    Co-authored-by: Bryan Steven Muñoz Cruzado <bryan312@hotmail.com>

--- editar.php
+++ editar.php
@@ -49,9 +49,16 @@
         }
     }
 ?>
+<head>
 <?php
-    include("particiones/head.php");
+    include("particiones/head.php")
 ?>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+<!-- DEPENDENCIAS -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</head>
 
 <!-- Modal para Cancelar Accion -->
 <div class="modal animate__animated animate__jackInTheBox" id="myModal" tabindex="-1">
@@ -64,21 +71,18 @@
                 </button>
             </div>
             <div class="modal-body">
-                <p>¿Esta Seguro de Eliminar esta Inquietud?</p>
+                <p>¿Esta Seguro de cancelar la edicion?</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                <form action="inquietudes.php" method="POST">
-                    <input type="hidden" name="eliminar" id="deletee">
-                    <button type="submit" class="btn btn-danger">Si, Estoy seguro</button>
-                </form>
+                <a href="basedatos.php" class="btn btn-danger">Si, Estoy seguro</a>
             </div>
         </div>
     </div>
 </div>
 
 <!-- Modal para Validar Accion -->
-<div class="modal animate__animated animate__jackInTheBox" id="myModal" tabindex="-1">
+<div class="modal animate__animated animate__jackInTheBox" id="modalValidar" tabindex="-1">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -92,10 +96,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                <form action="inquietudes.php" method="POST">
-                    <input type="hidden" name="eliminar" id="deletee">
-                    <button type="submit" class="btn btn-danger">Si, Estoy seguro</button>
-                </form>
+                <a href="editar.php" class="btn btn-success">Si, Estoy seguro</a>
             </div>
         </div>
     </div>
@@ -120,7 +121,7 @@
     <div class="col-6">
         <div class="card">
             <div class="card-body text-left">
-                <form action="editar.php" method="POST" enctype="multipart/form-data">
+                <form id="formulario" method="POST" enctype="multipart/form-data">
                     <h3 class="titulo"><i class="fas fa-user"></i> Datos Personales</h3>
                     <div class="form-group">
                         <label class="col control-label parrafo">Nombres</label>
@@ -268,14 +269,20 @@
                     <!-- Boton -->
                     <div class="form-group">
                         <label class="col control-label">&nbsp;</label>
-                        <div class="col">
+                        <div class="col text-center">
                             <button type="submit" class="btn btn-primary"><i class="fas fa-book"></i> Actualizar Consulta</button>
-                            <a href="basedatos.php" class="btn btn-danger">Cancelar</a>
                         </div>
                     </div>
                 </form>
+                <button class="btn btn-danger" data-toggle="modal" data-target="#myModal">Cancelar</button>
             </div>
         </div>
     </div>
 </div>
+<script>
+    $('#formulario').submit(function(e){
+        e.preventDefault();
+        $('#modalValidar').modal();
+    });
+</script>
 <script src="public/js/verificacion.js"></script>
